84a9ce80e2f038f9363db26e4944da3c8fe84b87
Jonathan Sciarrabba <sciarrabba.j00@gmail.com>
2025-04-23 12:29:08 +0200

    Add ride/complete endpoint
    Signed-off-by: Jonathan Sciarrabba <sciarrabba.j00@gmail.com>

--- src/handlers/ride/mod.rs
+++ src/handlers/ride/mod.rs
@@ -1,5 +1,6 @@
 mod accept;
 mod create;
+mod complete;
 
 use crate::AppState;
 use axum::routing::post;
@@ -9,5 +10,6 @@ pub fn get_router(app_state: &AppState) -> Router {
     Router::new()
         .route("/create", post(create::handler))
         .route("/accept", post(accept::handler))
+        .route("/complete", post(complete::handler))
         .with_state(app_state.clone())
 }
