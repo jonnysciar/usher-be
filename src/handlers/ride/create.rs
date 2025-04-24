bd8e4c712eecb5a79f24ba87deda059f4671c5a7
Jonathan Sciarrabba <sciarrabba.j00@gmail.com>
2025-04-24 18:02:55 +0200

    Add 'ride_id' field in response of ride/create
    Signed-off-by: Jonathan Sciarrabba <sciarrabba.j00@gmail.com>

--- src/handlers/ride/create.rs
+++ src/handlers/ride/create.rs
@@ -1,15 +1,17 @@
 use crate::app_state::AppState;
 use crate::jwt_controller::AccessClaims;
-use crate::response::{Error, ErrorKind, Success};
+use crate::response::{Error, ErrorKind, SuccessWithPayload};
 use axum::{
     extract::{Json, State},
     http::StatusCode,
     response::Result,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use sqlx::types::Uuid;
 
-const INSERT_RIDE_QUERY: &str = "INSERT INTO rides(user_id, start_lat, start_lon, end_lat, end_lon) VALUES ($1, $2, $3, $4, $5)";
+const INSERT_RIDE_QUERY: &str = "INSERT INTO rides(user_id, start_lat, start_lon, end_lat, end_lon)
+                                 VALUES ($1, $2, $3, $4, $5)
+                                 RETURNING id";
 
 #[derive(Debug, Deserialize)]
 pub struct Payload {
@@ -19,12 +21,17 @@ pub struct Payload {
     end_lon: f64,
 }
 
+#[derive(Debug, Serialize)]
+pub struct ReplayPayload {
+    ride_id: String,
+}
+
 //TODO: Add log
 pub async fn handler(
     access_claims: AccessClaims,
     State(state): State<AppState>,
     Json(payload): Json<Payload>,
-) -> Result<(StatusCode, Json<Success>)> {
+) -> Result<(StatusCode, Json<SuccessWithPayload<ReplayPayload>>)> {
     if access_claims.driver {
         return Err(Error::new(ErrorKind::Unauthorized).into());
     }
@@ -32,20 +39,24 @@ pub async fn handler(
     let uuid =
         Uuid::try_parse(&access_claims.sub).map_err(|_| Error::new(ErrorKind::InvalidToken))?;
 
-    let n_rows = sqlx::query(INSERT_RIDE_QUERY)
+    let ride_id: Option<Uuid> = sqlx::query_scalar(INSERT_RIDE_QUERY)
         .bind(uuid)
         .bind(payload.start_lat)
         .bind(payload.start_lon)
         .bind(payload.end_lat)
         .bind(payload.end_lon)
-        .execute(state.pool.as_ref())
+        .fetch_optional(state.pool.as_ref())
         .await
-        .map_err(|_| Error::new(ErrorKind::Server))?
-        .rows_affected();
+        .map_err(|_| Error::new(ErrorKind::Server))?;
 
-    if n_rows != 1 {
-        return Err(Error::new(ErrorKind::FailedRideOperation).into());
-    }
-
-    Ok((StatusCode::OK, Json::from(Success::new())))
+    ride_id
+        .map(|r| {
+            (
+                StatusCode::OK,
+                Json::from(SuccessWithPayload::new(ReplayPayload {
+                    ride_id: r.hyphenated().to_string(),
+                })),
+            )
+        })
+        .ok_or(Error::new(ErrorKind::FailedRideOperation).into())
 }
